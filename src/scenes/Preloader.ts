5afddc73affa25620d7e32fa5d0ddb4e9e715c18
Patrick Calahan <github@pcal.net>
2024-09-03 18:58:57 -0700

    checkpoint

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,14 +1,11 @@
 import { Scene } from 'phaser';
 
-export class Preloader extends Scene
-
-{
-    constructor ()
-    {
+export class Preloader extends Scene {
+    constructor() {
         super('Preloader');
     }
 
-    init () {
+    init() {
         //  We loaded this image in our Boot Scene, so we can display it here
         this.add.image(512, 384, 'background');
 
@@ -16,29 +13,27 @@ export class Preloader extends Scene
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        this.load.on('progress', (progress) => {
-
+        this.load.on('progress', (progress: number) => {
             //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
             bar.width = 4 + (460 * progress);
 
         });
     }
 
-    preload () {
-        this.load.image('space', 'assets/space3.png');
+    preload() {
         this.load.image('diamond', 'assets/diamond.png');
-        this.load.image('grass', 'assets/grass.png');    
+        this.load.image('grass', 'assets/grass.png');
         this.load.image('bank', 'assets/bank.png');
-        this.load.image('house', 'assets/house.png');    
+        this.load.image('house', 'assets/house.png');
         this.load.image('red', 'assets/red.png');
         this.load.image('sky', 'assets/sky.png');
         this.load.spritesheet('unicorn', 'assets/unicorn.png', { frameWidth: 16, frameHeight: 16, spacing: 1 });
     }
 
-    create () {
+    create() {
         //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, you can define global animations here, so we can use them in other scenes.
 
