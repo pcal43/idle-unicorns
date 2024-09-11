62307b62eaa59bf0d958661a80c0faed5c00ea1a
Patrick Calahan <github@pcal.net>
2024-09-10 21:11:11 -0700

    checkpoint

--- etc/release.sh
+++ etc/release.sh
@@ -2,10 +2,15 @@
 
 BASEDIR=$(git rev-parse --show-toplevel)
 TEMPDIR=$(mktemp -d)
-cd "${BASEDIR}" && npm run build
+cd "${BASEDIR}"
+rm -rf dist
+npm run build
 echo "Temp dir: ${TEMPDIR}"
 cd "${TEMPDIR}"
-git -C "${TEMPDIR}" clone --single-branch --branch pages  https://github.com/pcal43/idle-unicorns.git
+git clone --single-branch --branch pages  https://github.com/pcal43/idle-unicorns.git "${TEMPDIR}"
 cp "${BASEDIR}"/dist/* "${TEMPDIR}"
+git -C "${TEMPDIR}" config http.postBuffer 524288000
 git -C "${TEMPDIR}" add .
+git -C "${TEMPDIR}" commit -m 'publish'
+git -C "${TEMPDIR}" push
 
